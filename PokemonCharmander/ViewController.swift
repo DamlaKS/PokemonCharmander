1574a01da45a8bed497481f6cab61b8288ca9b11
Damla KS <damlaksrbst@gmail.com>
2023-04-16 23:51:07 +0300

    dark/light mode and faceID

--- PokemonCharmander/ViewController.swift
+++ PokemonCharmander/ViewController.swift
@@ -6,14 +6,117 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
+    var myImage = UIImageView()
+    var myLabel = UILabel()
+    let myButton = UIButton()
+    let againButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        starter()
+        
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            myButton.setTitleColor(.red, for: .normal)
+            againButton.setTitleColor(.white, for: .normal)
+            view.backgroundColor = .black
+        } else {
+            myButton.setTitleColor(.red, for: .normal)
+            againButton.setTitleColor(.black, for: .normal)
+            view.backgroundColor = .yellow
+        }
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        myImage.image = nil
+        myImage.frame = CGRect(x: width * 0.5 - 120, y: height * 0.5 - 250, width: 240, height: 240)
+        view.addSubview(myImage)
+        
+        myLabel.text = ""
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5, width: width * 0.8, height: 50)
+        view.addSubview(myLabel)
+        
+        myButton.setTitle("Start", for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.6, width: 200, height: 100)
+        view.addSubview(myButton)
+        againButton.setTitle("Try Again FaceID", for: UIControl.State.normal)
+        againButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.6, width: 200, height: 100)
+        view.addSubview(againButton)
+        
+        againButton.isHidden = true
+        againButton.addTarget(self, action: #selector(starter), for: .touchUpInside)
+        myButton.isHidden = true
+        myButton.addTarget(self, action: #selector(myAction), for: .touchUpInside)
     }
-
-
+    
+    @objc func starter() {
+        let authContext = LAContext()
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                if success == true {
+                    //successful auth
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Success!"
+                        self.myButton.isHidden = false
+                        self.againButton.isHidden = true
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error!"
+                        self.myButton.isHidden = true
+                        self.againButton.isHidden = false
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    @objc func myAction() {
+        if let image = myImage.image {
+            if image == UIImage(named: "charmander") {
+                myImage.image = UIImage(named: "charmeleon")
+                myLabel.text = "charmeleon"
+                myButton.setTitle("get bigger", for: .normal)
+            } else if image == UIImage(named: "charmeleon") {
+                myImage.image = UIImage(named: "charizard")
+                myLabel.text = "charizard"
+                myButton.setTitle("start over", for: .normal)
+            } else if image == UIImage(named: "charizard") {
+                myImage.image = UIImage(named: "charmander")
+                myLabel.text = "charmander"
+                myButton.setTitle("get bigger", for: .normal)
+            }
+        } else {
+            myImage.image = UIImage(named: "charmander")
+            myLabel.text = "charmander"
+            myButton.setTitle("get bigger", for: .normal)
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        starter()
+        viewDidLoad()
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            myButton.setTitleColor(.red, for: .normal)
+            againButton.setTitleColor(.white, for: .normal)
+            view.backgroundColor = .black
+        } else {
+            myButton.setTitleColor(.red, for: .normal)
+            againButton.setTitleColor(.black, for: .normal)
+            view.backgroundColor = .yellow
+        }
+    }
+    
 }
-
